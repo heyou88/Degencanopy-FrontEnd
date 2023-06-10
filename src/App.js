4210180c3e26ddfa85eb83767539b7480a7d4005
you he <heyou88@hotmail.com>
2023-06-10 22:31:48 +0200

    transparent background

--- src/App.js
+++ src/App.js
@@ -3,43 +3,16 @@ import Sidebar from './components/Sidebar';
 import Page from './components/Page';
 import './app.css';
 
-// const App = () => {
-//   const [currentPage, setCurrentPage] = useState('MY TREES');
-
-//   const setPage = (page) => {
-//     setCurrentPage(page);
-//   };
-
-//   return (
-//     <div className="app">
-//       <Sidebar setPage={setPage} />
-//       <Page currentPage={currentPage} />
-//     </div>
-//   );
-// };
-
-// export default App;
 const App = () => {
   const [currentPage, setCurrentPage] = useState('MY TREES');
-  const [sidebarVisible, setSidebarVisible] = useState(true);
-
-  const handleMouseEnter = () => {
-    setSidebarVisible(true);
-  };
-
-  const handleMouseLeave = (event) => {
-    if (event.pageX < 50) {
-      setSidebarVisible(false);
-    }
-  };
 
   const setPage = (page) => {
     setCurrentPage(page);
   };
 
   return (
-    <div className="app" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      <Sidebar setPage={setPage} hidden={!sidebarVisible} />
+    <div className="app" >
+      <Sidebar setPage={setPage}/>
       <Page currentPage={currentPage} />
     </div>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import './sidebar.css';
-const Sidebar = ({ setPage }) => {
-  const pages = ['GET A TREE', 'MY TREES', 'GET WATER', 'LOTTERY']; // List of pages
+import React, { useState } from 'react';
+import '../app.css';
 
+  const Sidebar = ({ setPage }) => {
+    const [sidebarVisible, setSidebarVisible] = useState(true);
+    const pages = ['GET A TREE', 'MY TREES', 'GET WATER', 'LOTTERY']; // List of pages
+
+    const handleMouseEnter = () => {
+      setSidebarVisible(true);
+    };
+
+    const handleMouseLeave = (event) => {
+      const { clientX, clientY } = event;
+
+      const appContainer = document.querySelector('.app');
+      const { left, top, width, height } = appContainer.getBoundingClientRect();
+
+      if (
+        clientX < left ||
+        clientX > left + width ||
+        clientY < top ||
+        clientY > top + height
+      ) {
+        setSidebarVisible(false);
+      }
+    };
   return (
-    <div className="sidebar">
+    <div  className={`sidebar ${sidebarVisible ? 'open' : ''}`}
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}>
       {pages.map((page, index) => (
         <button key={index} onClick={() => setPage(page)}>
           {page}

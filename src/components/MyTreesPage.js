1c4abdda25678f358c3e6f607fe5ad7577bcc556
you he <heyou88@hotmail.com>
2023-06-11 11:58:00 +0200

    finish water tree page

--- src/components/MyTreesPage.js
+++ src/components/MyTreesPage.js
@@ -54,34 +54,59 @@ import './page.css';
 import treeData from './treedata1';
 
 const MyTreesPage = () => {
-  const [imageId, setImageId] = useState(1); // Initialize with 1 as the starting image ID
+  const [treeStates, setTreeStates] = useState({
+    blueTree: { imageId: 1 },
+    greenTree: { imageId: 1 },
+    redTree: { imageId: 1 },
+  });
 
-  const handleWaterClick = () => {
-    setImageId((prevId) => (prevId < 3 ? prevId + 1 : prevId)); // Increment the image ID from 1 to 3 and stop at 3
+  const handleWaterClick = (treeName) => {
+    setTreeStates((prevTreeStates) => {
+      const prevImageId = prevTreeStates[treeName].imageId;
+      const newImageId = prevImageId < 3 ? prevImageId + 1 : prevImageId;
+
+      return {
+        ...prevTreeStates,
+        [treeName]: {
+          imageId: newImageId,
+        },
+      };
+    });
+  };
+
+  const renderTree = (tree, treeName) => {
+    const { imageId } = treeStates[treeName];
+
+    return tree.map((treeItem) => {
+      if (treeItem.id === imageId) {
+        return (
+          <div className="tree-card" key={treeItem.id}>
+            <img src={treeItem.image} className="tree-image" alt="Tree" />
+            <button className="water-button" onClick={() => handleWaterClick(treeName)}>
+              Water It
+            </button>
+          </div>
+        );
+      }
+      return null;
+    });
   };
 
   const blueTree = treeData.blueTree;
+  const greenTree = treeData.greenTree;
+  const redTree = treeData.redTree;
 
   return (
     <div className="My-trees-page">
       <h1 className="My-tree-headline">MY TREES</h1>
       <div className="tree-container">
-        {blueTree.map((tree) => {
-          if (tree.id === imageId) {
-            return (
-              <div className="tree-card" key={tree.id}>
-                <img src={tree.image} className="tree-image" alt="Tree" />
-                <button className="water-button" onClick={handleWaterClick}>
-                  Water It
-                </button>
-              </div>
-            );
-          }
-          return null;
-        })}
+        {renderTree(blueTree, 'blueTree')}
+        {renderTree(greenTree, 'greenTree')}
+        {renderTree(redTree, 'redTree')}
       </div>
     </div>
   );
 };
 
 export default MyTreesPage;
+
